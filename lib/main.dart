b77e1df4c39b6a198fb28583ce8933fd84930d65
Samu <zamuelfernandes@gmail.com>
2022-05-19 09:16:04 -0300

    Creating the initial layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
@@ -9,6 +7,7 @@ import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Home(),
   ));
 }
@@ -22,19 +21,66 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List _toDoList = [];
+  Color baseColor = Colors.purpleAccent;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: baseColor,
+        title: const Text('To-Do List'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.fromLTRB(17, 1, 7, 1),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: "Nova Tarefa",
+                      labelStyle: TextStyle(color: baseColor),
+                    ),
+                  ),
+                ),
+                RaisedButton(
+                  onPressed: () {},
+                  color: baseColor,
+                  child: Text('ADD'),
+                  textColor: Colors.white,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
+  //GET THE FILE ----------------------------------------------------
   //Future<File> _getFile() async {
   Future _getFile() async {
     final directory = await getApplicationDocumentsDirectory();
     return File("${directory.path}/data.json");
   }
 
+  //SAVE THE FILE ----------------------------------------------------
   Future _saveData() async {
     String data = json.encode(_toDoList);
+    final file = await _getFile();
+
+    return file.writeAsString(data);
+  }
+
+  //READ THE FILE ----------------------------------------------------
+  Future _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
   }
 }
